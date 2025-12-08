c37d88986d3db9ee1b04bf502258dca5dd4fe835
AhmedMahmoud4466 <ahmedmahmoud952004@gmail.com>
2025-12-08 22:58:31 +0200

    Update comp_decomp.h

--- comp_decomp.h
+++ comp_decomp.h
@@ -1,5 +1,5 @@
-#ifndef comp_decomp
-#define comp_decomp
+#ifndef COMP_DECOMP
+#define COMP_DECOMP
 
 #include <string>
 #include <vector>
@@ -13,25 +13,13 @@ string readFile(const string& filename);
 vector<uint16_t> stringToU16(const string& s);
 string u16ToString(const vector<uint16_t>& v);
 
-void bytePairEncode(
-    vector<uint16_t>& data,
-    map<uint16_t, pair<uint16_t,uint16_t>>& dict,
-    int maxSymbols = 512
-);
-void bytePairDecode(
-    vector<uint16_t>& data,
-    const map<uint16_t, pair<uint16_t,uint16_t>>& dict
-);
+void bytePairEncode(vector<uint16_t>& data, map<uint16_t, pair<uint16_t,uint16_t>>& dict, int maxSymbols = 512);
+void bytePairDecode(vector<uint16_t>& data, const map<uint16_t, pair<uint16_t,uint16_t>>& dict);
 
-void writeCompressed(
-    const string& filename,
-    const vector<uint16_t>& data,
-    const map<uint16_t, pair<uint16_t,uint16_t>>& dict
-);
-void readCompressed(
-    const string& filename,
-    vector<uint16_t>& data,
-    map<uint16_t, pair<uint16_t,uint16_t>>& dict
-);
+void writeCompressed(const string& filename, const vector<uint16_t>& data, const map<uint16_t, pair<uint16_t,uint16_t>>& dict);
+void readCompressed(const string& filename, vector<uint16_t>& data, map<uint16_t, pair<uint16_t,uint16_t>>& dict);
+
+void compress(const string& inputFile, const string& outputFile);
+void decompress(const string& inputFile, const string& outputFile);
 
 #endif
