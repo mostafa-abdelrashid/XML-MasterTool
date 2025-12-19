27bb4cdd7a48f21782e6dcbed73e74d917d6c8f3
NevinYasser <nevinyasser52004@gmail.com>
2025-12-19 18:48:50 +0200

    User.cpp
    Adding some functions

--- Classes/User.cpp
+++ Classes/User.cpp
@@ -39,6 +39,24 @@ const vector<User*>& User::getFollowers() const {
 	return followers;
 }
 
+const vector<int>& User::getFollowingIDs() const {
+	return following_id;
+}
+const vector<User*>& User::getFollowing() const { 
+}
+
+int User::getFollowersCount() const { 
+	return followers_id.size();
+}
+
+int User::getFollowingCount() const { 
+	return following_id.size();
+}
+
+int User::getTotalConnections() const { 
+	return followers_id.size() + following_id.size();
+}
+
 void User::setName(string name) {
 	this->name = name;
 }
@@ -47,12 +65,22 @@ void User::addPost(const Post& newPost) {
 	posts.push_back(newPost);
 }
 
-void User::addFollowerID(int follower_id) {
-	followers_id.push_back(follower_id);
+void User::addFollowerID(int follower_ID) {
+	followers_id.push_back(follower_ID);
 }
 
 void User::addFollower(User* newFollower_ptr) {
 	if (newFollower_ptr != nullptr) {
 		followers.push_back(newFollower_ptr);
+	}
+}
+
+void User::addFollowingID(int following_ID) { 
+	following_id.push_back(following_ID);
+}
+
+void User::addFollowing(User* newFollowing_ptr) { 
+	if (newFollowing_ptr != nullptr) {
+		following.push_back(newFollowing_ptr);
 	}
 }
