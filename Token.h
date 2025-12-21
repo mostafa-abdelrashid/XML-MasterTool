6aab18db76b884db1f4d2fea1273e4f942492d33
ahmed-mo690 <ahmed.mohamed18004@gmail.com>
2025-12-21 13:57:28 +0200

    Enhance Token.h with XMLNode and error handling
    Added line number to Token structure and introduced XMLNode and XMLError structures.

--- Token.h
+++ Token.h
@@ -1,7 +1,12 @@
 #ifndef TOKEN_H_INCLUDED
 #define TOKEN_H_INCLUDED
-#include <string>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <memory>
+#include <fstream>
+#include <sstream>
 using namespace std;
 // Define the types of tokens
 enum TokenType {
@@ -12,8 +17,19 @@ UNKNOWN };
 struct Token {
     TokenType type;
     string value;
+    int line;
+};
+struct XMLNode {
+    string tagName;
+    string value;
+    vector<shared_ptr<XMLNode>> children;
+    XMLNode(string name) : tagName(name) {}
 };
 
+struct XMLError {
+    int line;
+    string message;
+};
 
 #endif
 
