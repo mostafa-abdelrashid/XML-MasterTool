7698a492f0989fe373fb3d3e118054298fefc0bb
ahmed-mo690 <ahmed.mohamed18004@gmail.com>
2025-12-22 11:21:19 +0200

    Refactor XMLTokenizer to use a class structure

--- XMLTokenizer.h
+++ XMLTokenizer.h
@@ -3,11 +3,18 @@
 
 #include "Token.h"
 
-extern std::vector<Token> g_tokens;
+class XMLTokenizer {
+private:
+    vector<Token> tokens;
+    string trim(const string& str);
 
+public:
+    vector<Token> tokenize(string filePath);
+    const vector<Token>& getTokens() const;
+
+};
 string readFile(const string& filename);
-string trim(const string& str);
-
-vector<Token> tokenize(string filePath);
+extern XMLTokenizer g_tokenizer;
 void Tokens(const string& inputFile);
 #endif
+
